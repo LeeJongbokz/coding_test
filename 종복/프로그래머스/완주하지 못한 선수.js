1931f16126bb2404470d6d83ccc4f8589f1a98e6
LeeJongbok <s2feeling@naver.com>
2022-02-06 03:02:00 +0900

    Create 완주하지 못한 선수.js

---
+++ 종복/프로그래머스/완주하지 못한 선수.js
@@ -0,0 +1,29 @@
+function solution(participant, completion) {
+    var answer = '';
+    
+    let map = new Map();
+    
+    for(let i=0; i<participant.length; i++){
+        if(map.has(participant[i])){
+            map.set(participant[i], map.get(participant[i])+1);
+        }else{
+            map.set(participant[i], 1);
+        }
+    }
+    
+    for(let i=0; i<completion.length; i++){
+        if(map.has(completion[i])){
+            map.set(completion[i], map.get(completion[i])-1);
+        }
+    }
+    
+    
+    for(let [key, val] of map){
+        if(val === 1){
+            answer = key;
+            break;
+        }
+    }
+    
+    return answer;
+}
