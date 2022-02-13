37a637318225d7aba39085bcb073d9363f81c343
LeeJongbok <s2feeling@naver.com>
2022-02-13 15:48:48 +0900

    Create 최솟값 만들기.js

---
+++ 종복/3-1주차/최솟값 만들기.js
@@ -0,0 +1,19 @@
+// 10분
+// 1sol
+
+function solution(A,B){
+    var answer = 0;
+
+    A.sort(function(a,b){
+       return a-b;
+    });
+    B.sort(function(a,b){
+       return b-a;
+    });
+    
+    for(let i=0; i<A.length; i++){
+        answer += A[i]*B[i];
+    }
+
+    return answer;
+}
