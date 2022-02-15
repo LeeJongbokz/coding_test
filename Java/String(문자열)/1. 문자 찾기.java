540ea9644e2cb35892672b36b91f119fcab2c832
LeeJongbok <s2feeling@naver.com>
2022-02-15 19:29:16 +0900

    Update 1. 문자 찾기.java

--- Java/String(문자열)/1. 문자 찾기.java
+++ Java/String(문자열)/1. 문자 찾기.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
   
 public class Main {
   public int solution(String str, char t){
-	     	int answer = 0; 
+	
+	int answer = 0; 
         str = str.toUpperCase();
         t = Character.toUpperCase(t);
         
@@ -14,13 +15,13 @@ public class Main {
     
         return answer;
     
-	}
+  }
 
 	public static void main(String[] args){
-		Main T = new Main();
-		Scanner kb = new Scanner(System.in);
-		String str=kb.next();
-		char c=kb.next().charAt(0);
-		System.out.print(T.solution(str, c));
+	    Main T = new Main();
+	    Scanner kb = new Scanner(System.in);
+	    String str=kb.next();
+	    char c=kb.next().charAt(0);
+	    System.out.print(T.solution(str, c));
 	}
 }
