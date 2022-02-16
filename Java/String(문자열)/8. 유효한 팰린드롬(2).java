3eea1c860fc25d37c2b664e18fb8d7104cfa5e3e
LeeJongbok <s2feeling@naver.com>
2022-02-16 19:55:22 +0900

    Create 8. 유효한 팰린드롬(2).java

---
+++ Java/String(문자열)/8. 유효한 팰린드롬(2).java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {	
+	public String solution(String s){
+	 	String answer="NO";
+        s = s.toLowerCase();
+        String tmp = s.replaceAll("[^a-z]", "");
+        String reverseTmp = new StringBuilder(tmp).reverse().toString();
+        
+        if(tmp.equals(reverseTmp)){
+           answer = "YES"; 
+        }
+        
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.nextLine();
+		System.out.print(T.solution(str));
+	}
+}
