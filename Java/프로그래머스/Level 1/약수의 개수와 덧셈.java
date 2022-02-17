4fbaa004a9bc7b0feacee2816bf9144522727aef
LeeJongbok <s2feeling@naver.com>
2022-02-17 17:10:06 +0900

    Create 약수의 개수와 덧셈.java

---
+++ Java/프로그래머스/Level 1/약수의 개수와 덧셈.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public int getDivideNumbers(int n){
+        
+        int cnt = 0; 
+        
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                cnt += 1;
+            }
+        }
+        return cnt; 
+    }
+    
+    
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i=left; i<=right; i++){
+            if(getDivideNumbers(i) % 2 == 0){
+                answer += i;
+            }else{
+                answer -=i;
+            }
+        }
+        
+        return answer;
+    }
+}
