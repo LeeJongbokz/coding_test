da63c91a11e2c9c5cd7e0995126ead640c6da047
LeeJongbok <s2feeling@naver.com>
2022-02-25 21:19:12 +0900

    Create 7. 회문 문자열(3).java

---
+++ Java/String(문자열)/7. 회문 문자열(3).java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {	
+	public String solution(String str){
+		String answer="YES";
+        str = str.toLowerCase();
+      
+      	String tmp = new StringBuilder(str).reverse().toString();
+      
+        if(!str.equals(tmp)){
+           answer = "NO";
+        }
+      
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}
+
