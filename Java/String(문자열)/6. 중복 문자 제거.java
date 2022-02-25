b30e1e86fa10775e7410d7cdfd60d55ded7c7f6d
LeeJongbok <s2feeling@naver.com>
2022-02-25 21:08:40 +0900

    Create 6. 중복 문자 제거.java

---
+++ Java/String(문자열)/6. 중복 문자 제거.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {	
+	public String solution(String str){
+      
+      String answer = "";
+      
+      for(int i=0; i<str.length(); i++){
+        char c = str.charAt(i);
+        if(i == str.indexOf(c)){
+           answer += Character.toString(c);
+        }
+      }
+      
+      return answer;
+      
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}
